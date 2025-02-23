f934a7a118c727297b49d72c1901b825a787dae0
Martin Malotky <mmalotky@gmail.com>
2025-02-23 18:15:20 -0500

    more emoji mapping

--- src/utility/GroupMeEmojiMap.ts
+++ src/utility/GroupMeEmojiMap.ts
@@ -9,6 +9,20 @@ export const emojiMap = [
     [
         ":slightly_smiling_face:", ":grinning:", ":smile:", ":blush:", ":wink:", ":smiley:", ":smiling_face_with_3_hearts:", ":clown_face:",
         ":zany_face:", ":yum:", ":disguised_face:", ":nerd:", ":sunglasses:", ":cowboy:", ":slight_frown:", ":frowning_face:",
-        ":persevere:", ":cry:", ":sob:", ":sweat:", ":angry:", ":rage:", ":smiling_imp:", ":face_with_raised_eyebrow:"
-    ]
+        ":persevere:", ":cry:", ":sob:", ":sweat:", ":angry:", ":rage:", ":smiling_imp:", ":face_with_raised_eyebrow:", ":face_with_spiral_eyes:",
+        ":face_vomiting:", ":dizzy_face:", ":weary_face:", ":frowning_face:", ":frowning_face:", ":fearful_face:", 
+        ":neutral_face:", ":flushed_face:", ":nerd_face:", ":face_with_open_eyes_and_hand_over_mouth:", ":face_with_peeking_eye:", ":neutral_face:", 
+        ":zipper_mouth_face:", ":face_exhaling:", ":melting_face:", ":neutral_face:", ":shaking_face:", ":man_beard:", ":billed_cap:", ":nerd_face:",
+        ":cup_with_straw:", ":hot_beverage:", ":cookie:", ":lotion_bottle:", ":taco:", ":pizza:", ":bottle_with_popping_cork:", ":sake:", ":glass_of_milk:",
+        ":bubble_tea:", ":ping_pong:", ":joystick:", ":guitar:", ":hot_beverage:", ":bone:", ":dog_face:", ":chipmunk:", ":t_rex:", ":heart:",
+        ":100:", ":bread:", ":grinning_cat:", ":pouting_cat:", ":grinning_cat:", ":grinning_cat:", ":grinning_cat:", 
+        ":grinning_cat:", ":grinning_cat:", ":grinning_cat:", ":grinning_cat:", ":grinning_cat:", ":grinning_cat:", ":grinning_cat:", ":grinning_cat:",
+        ":kissing_cat:", ":grinning_cat:", ":grinning_cat:", ":grinning_cat:", ":grinning_cat:"
+    ],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
 ]

bcb7fdf9417dc4c807ad4d4e2e9874cba70de0d6
RWDJ <github.64@rwdj.info>
2025-12-29 21:52:41 -0600

    Fix workflow to test maintainer's pull requests

--- src/test.ts
+++ src/test.ts
@@ -3,6 +3,7 @@ import * as Bot from "./handlers/BotHandler.js";
 
 async function main() {
   console.log(`TESTING: ${process.env.TEST_GROUPME_GROUP_NAME}`);
+  console.log(`TESTING: asdfghjkl`);
   dotenv.config();
   await Bot.runAndExit();
   console.log("234r5yuh7");
